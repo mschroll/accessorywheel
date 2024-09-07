573b96d57c6a14ce54aabedcac2c4ee49144e456
svnrise <159047360+mschroll@users.noreply.github.com>
2024-09-07 22:10:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'bodacious'
 game 'gta5'
-author 'dullpear'
-version '1.0.3'
-description 'dpClothing+'
+author 'dullpear & veitheim'
+version '1.0.0'
+description 'dpClothing+ Edit'
 
 client_scripts {
 	'Client/Functions.lua', 		-- Global Functions / Events / Debug and Locale start.
